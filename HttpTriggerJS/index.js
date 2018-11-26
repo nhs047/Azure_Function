a8e9bf3062a6908910f764a6eb2afe50fd9637e8
Nobi Hossain <nhossain@era.com.bd>
2018-11-26 14:50:23 +0600

    Modified

--- HttpTriggerJS/index.js
+++ HttpTriggerJS/index.js
@@ -1,23 +1,68 @@
-module.exports = function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: {message: "Hello " + (req.query.name || req.body.name)},
-            headers: {
-                'Content-Type': 'application/json'
-            }
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+var TYPES = require('tedious').TYPES;
+module.exports = function (context) {
+    var _retArray = [];
+    var _currentData = {};
+try {
+        var config = {
+            userName: 'nobihossain',
+            password: 'Onto@123',
+            server: 'serverlesspoc.database.windows.net',
+            options: {encrypt: true, database: 'serverless'}
         };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: {message: "Please pass a name on the query string or in the request body"},
-            headers: {
-                'Content-Type': 'application/json'
+        var connection = new Connection(config);
+        connection.on('connect', () => {
+            request = new Request("SELECT * FROM ServerLessPoc;", (err)=>{
+                if(err) {context.res = returnObj(
+                    400, 
+                    {
+                        message: ex.message
+                    }
+                );
+                context.done();
             }
-        };
+            });
+            request.on('row', function(columns) {
+                _currentData.id = columns[0].value;
+                _currentData.firstName = columns[1].value;
+                _currentData.lastName = columns[2].value;
+                _currentData.dateOfBirth = columns[3].value;
+                _currentData.passportId = columns[4].value;
+                _currentData.nId = columns[5].value;
+                _currentData.createdAt = columns[6].value;
+                _retArray.push(_currentData);
+            });
+            request.on('requestCompleted', () => {
+                context.res = returnObj(
+                    200, 
+                    {
+                        data: _retArray
+                    }
+                );
+                context.done();
+            })
+            connection.execSql(request);
+        });
     }
+    catch (ex) {
+        context.res = returnObj(
+            400, 
+            {
+                message: ex.message
+            }
+        );
     context.done();
+
+    }
+
+    returnObj = (status, body) => {
+        return {
+                status,
+                body,
+                headers : {
+                    'Content-Type': 'application/json'
+                }
+        }
+    }
 };
