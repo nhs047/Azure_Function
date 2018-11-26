c6e96644ffd8f9949876fe6741ec67544e338eb1
Nobi Hossain <nhossain@era.com.bd>
2018-11-26 10:17:33 +0600

    init

---
+++ HttpTriggerJS/index.js
@@ -0,0 +1,23 @@
+module.exports = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: {message: "Hello " + (req.query.name || req.body.name)},
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: {message: "Please pass a name on the query string or in the request body"},
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+    }
+    context.done();
+};
